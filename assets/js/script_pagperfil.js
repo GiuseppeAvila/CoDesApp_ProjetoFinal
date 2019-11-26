09b547cd5c52c10124bd6c714707b773a49b23d7
carolinechaim <caroline.chaim@gmail.com>
2019-11-26 08:52:56 -0300

    java

---
+++ assets/js/script_pagperfil.js
@@ -0,0 +1,60 @@
+/*
+  É muito importante que todo seu código fique dentro deste
+  document.addEventListener, pois isso garante que ele vai
+  rodar apenas quando toda a página estiver carregada. Isso
+  foi explicado no Material Prévio de JavaScript da Aula 9.
+*/
+document.addEventListener('DOMContentLoaded', function() {
+
+  /*
+    Cria uma conexão com o Firebase.
+  */
+
+  let db = coDesConnect("https://projetofinal-4ecc0.firebaseio.com/")
+
+  /*
+    Captura os parametros enviados para a pagina
+  */
+
+  let parametros = coDesExtract()
+ 
+  /*
+    Define a categoria da paginas
+  */
+  let perfil;
+
+  if (parametros.hasOwnProperty("perfil")) {
+    perfil = parametros["perfil"]
+  }
+
+  /*
+   Muda os valores dentro do html.
+  */
+
+  db.download("telainical", function(data) {
+
+    /*
+      Verifica se a data contem a categoria em questão
+    */
+
+    if (!data.hasOwnProperty(perfil)) {
+      console.log("Erro -> Perfil não idendificada");
+      
+      categoria = "fisico"
+    }
+    categoria = categoria.toLowerCase()
+
+    /*
+      Muda o titulo da pagina
+    */
+
+    window.document.title = categoria.charAt(0).toUpperCase() + categoria.slice(1)
+
+    coDesReplace(".h2_b1", categoria)
+    coDesReplace(".texto1", data[categoria])
+
+    document.body.innerHTML = document.body.innerHTML.replace(/qual_categoria/g, categoria)
+
+    coDesReplace(".projetos", data[categoria])
+  })
+})
